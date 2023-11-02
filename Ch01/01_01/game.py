745882b141eb90d1245836a2e6b0439d3c1291a5
Miki Tebeka <miki@353solutions.com>
2023-11-02 09:55:36 +0000

    code comments

--- Ch01/01_01/game.py
+++ Ch01/01_01/game.py
@@ -1,9 +1,9 @@
 # %% Player
 class Player:
-    num_players = 0
+    num_players = 0  # Class attribute
 
     def __init__(self, name):
-        self.name = name
+        self.name = name  # Instance attribute
         self.mana = 100
         self.num_players += 1
         print('self:', self.num_players)
